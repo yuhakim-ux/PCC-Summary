8a2f1b67c7337d9e2a68cbe8b1d58caad10884b7
UHA-UX <u.ha.interaction@gmail.com>
2026-04-10 01:02:24 -0700

    chore: remove starter kit scaffolding unrelated to AHIS prototype

--- prototype/src/modules/ui/example/example.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-/**
- * Example ui component. Use as <ui-example> in templates.
- * Duplicate this folder and rename for new ui components (e.g. ui-card, ui-modal).
- */
-export default class Example extends LightningElement {
-    @api title = 'Example';
-}
